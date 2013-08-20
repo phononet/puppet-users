4bf4ff0f8263c54fe8446f53438a8faf6ba9ec34
Siggi Wanke <swanke@phononet.de>
2013-08-20 17:48:51 +0200

    [BUGFIX] Don't create prefix home dir if ensure parameter is set 'absent'

--- manifests/home.pp
+++ manifests/home.pp
@@ -5,7 +5,7 @@ define users::home (
 )
 {
   include users::params
-
+  # Don't remove root directory
   if ( $title == 'root' ) {
     $home_real        = '/root'
     $ensure_directory = 'directory'
@@ -23,27 +23,30 @@ define users::home (
       default   => $ensure,
     }
   }
-
+  # 
   if $ensure == 'absent' {
-    $group = undef
-    $owner = undef
+    $group        = undef
+    $owner        = undef
+    $require_home = undef
   } else {
-    $group = $title
-    $owner = $title
+    $group        = $title
+    $owner        = $title
+    $require_home = Exec [ "${title}_prefix_home" ]
+    # Create prefix dir
+    exec { "${title}_prefix_home":
+      command   => "mkdir -p ${home_real}",
+      logoutput => true,
+      onlyif    => "test ! -d ${home_real}",
+    }
   }
 
-  exec { "${title}_prefix_home":
-    command   => "mkdir -p ${home_real}",
-    logoutput => true,
-    onlyif    => "test ! -d ${home_real}",
-  }
-
+  # Create home dir
   file { $home_real:
     ensure  => $ensure_directory,
     owner   => $owner,
     group   => $group,
     force   => $force,
     mode    => $mode_real,
-    require => Exec [ "${title}_prefix_home" ],
+    require => $require_home,
   }
 }
